dd3699cff364ea5c25933689ddd89cb19f909e8d
Rgee92 <randallcgee92@gmail.com>
2024-02-15 18:57:11 -0800

    add code to serial.py

--- python/python-oo-practice/serial.py
+++ python/python-oo-practice/serial.py
@@ -20,3 +20,17 @@ class SerialGenerator:
     100
     """
 
+    def _init_(self, start):
+        """Creates a new instance of Serial Generator, starting value set to passed in parameter and generate count set to zero."""
+        self._serial_number = start
+        self.count = 0
+    
+    def generate(self):
+        """Returns the sum of the generator's start value and the current count of generate() calls."""
+        return_value = self.start_value + self.count
+        self.count += 1
+        return  return_value
+    
+    def reset(self):
+        """Resets the counter of generate() calls to zero."""
+        self.count = 0
